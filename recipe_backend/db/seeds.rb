03e2a94c592cdca4727809b0f752eae388c15002
sydnee510 <sbsydnee161416@gmail.com>
2020-04-26 00:56:02 -0700

    add seed data

--- recipe_backend/db/seeds.rb
+++ recipe_backend/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+pound_cake = Recipe.create(title: "pound cake")
+krispy_treats = Recipe.create(title: "krispy treats")
+apple_pie = Recipe.create(title: "apple pie")
+
+flour = Ingredient.create(name: "flour", quantity: 4, recipe: pound_cake)
+eggs = Ingredient.create(name: "eggs", quantity: 6, recipe: pound_cake)
+
+marshmellows = Ingredient.create(name: "marshmellows", quantity: 10, recipe: krispy_treats)
+rice_krispies = Ingredient.create(name: "rice_krispies", quantity: 2, recipe: krispy_treats)
+
+apples = Ingredient.create(name: "apples", quantity: 6, recipe: apple_pie)
+sugar = Ingredient.create(name: "sugar", quantity: 1 , recipe: apple_pie)
+
+
+
+
